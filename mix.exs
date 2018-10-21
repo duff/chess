6eddfa9966096f61b34027401361a9d4f1363f2d
Duff O'Melia <duff@omelia.org>
2018-10-21 07:21:57 -0400

    Make the project an OTP app
    As if --sup had been given when the project was created.

--- mix.exs
+++ mix.exs
@@ -11,14 +11,13 @@ defmodule Chess.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {Chess.Application, []}
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
